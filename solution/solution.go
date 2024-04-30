ec6ff96dd3efde92d6dd43982955409953f4ab3f
Mikey Ferguson <90787330+Pirate-Hunter-Zoro@users.noreply.github.com>
2024-04-30 15:25:49 -0500

    skeleton for combinationSum(4)

--- solution/solution.go
+++ solution/solution.go
@@ -2794,3 +2794,14 @@ func topDownCanIWin(available int, maxChoosableInteger int, desiredTotal int, so
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+/*
+Given an array of distinct integers nums and a target integer target, return the number of possible combinations that add up to target.
+
+The test cases are generated so that the answer can fit in a 32-bit integer.
+
+Link:
+https://leetcode.com/problems/combination-sum-iv/description/
+*/
+func combinationSum(nums []int, target int) int {
+    return 0
+}
