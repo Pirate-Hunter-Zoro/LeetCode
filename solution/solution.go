722e06af2bd7f050e1c00b9b066b66770de34b00
Mikey Ferguson <90787330+Pirate-Hunter-Zoro@users.noreply.github.com>
2024-05-22 20:29:12 -0500

    skeleton for beautiful subsets

--- solution/solution.go
+++ solution/solution.go
@@ -4571,3 +4571,21 @@ func collectTheCoins(coins []int, edges [][]int) int {
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+/*
+You are given an array nums of positive integers and a positive integer k.
+
+A subset of nums is beautiful if it does not contain two integers with an absolute difference equal to k.
+
+Return the number of non-empty beautiful subsets of the array nums.
+
+A subset of nums is an array that can be obtained by deleting some (possibly none) elements from nums. Two subsets are different if and only if the chosen indices to delete are different.
+
+Link:
+https://leetcode.com/problems/the-number-of-beautiful-subsets/description/?envType=daily-question&envId=2024-05-23
+*/
+func beautifulSubsets(nums []int, k int) int {
+	return 0
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
