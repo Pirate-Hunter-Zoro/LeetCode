6392cbfd7eaf6c6767afb769aeef16a55e003e43
Mikey Ferguson <90787330+Pirate-Hunter-Zoro@users.noreply.github.com>
2024-05-29 19:08:46 -0500

    added skeleton for maxRotateFunction

--- solution/solution.go
+++ solution/solution.go
@@ -5168,3 +5168,21 @@ func findLowest1(binary_rep []bool) int {
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+/*
+You are given an integer array nums of length n.
+
+Assume arrk to be an array obtained by rotating nums by k positions clock-wise. We define the rotation function F on nums as follow:
+
+F(k) = 0 * arrk[0] + 1 * arrk[1] + ... + (n - 1) * arrk[n - 1].
+Return the maximum value of F(0), F(1), ..., F(n-1).
+
+The test cases are generated so that the answer fits in a 32-bit integer.
+
+Link:
+https://leetcode.com/problems/rotate-function/description/
+*/
+func maxRotateFunction(nums []int) int {
+    return 0
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
